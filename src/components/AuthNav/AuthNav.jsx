62762e0b91a2c7c567a4a3ffc0b71d04069a5810
Urszula Molska <urszula_molska@wp.pl>
2023-02-15 22:14:31 +0100

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -3,10 +3,10 @@ import css from './AuthNav.module.css';
 export const AuthNav = () => {
   return (
     <div className={css.wrapper}>
-      <NavLink className="navLink" to="/goit-react-hw-08-phonebook/register">
+      <NavLink className="navLink" to="/register">
         Register
       </NavLink>
-      <NavLink className="navLink" to="/goit-react-hw-08-phonebook/login">
+      <NavLink className="navLink" to="/login">
         Log In
       </NavLink>
     </div>
