252c5ae4408ccb25c7085ed4a57261ae94fb30dc
Urszula Molska <urszula_molska@wp.pl>
2023-03-05 19:52:13 +0100

    Update PrivateRoute.js

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from 'hooks/useAuth.js';
 
-/**
+/*
  * - If the route is private and the user is logged in, render the component
  * - Otherwise render <Navigate> to redirectTo
  */
